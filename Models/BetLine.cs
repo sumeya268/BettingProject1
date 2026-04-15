9df21409fd3be92e20a9258e9114dc3335c69adb
sumeya268 <SM3949@live.mdx.ac.uk>
2026-04-15 14:49:28 +0100

    editing
    Remove BetLine class definition from the BettingAPI.Models namespace.

--- Models/BetLine.cs
+++ Models/BetLine.cs
@@ -1,17 +1,1 @@
-﻿namespace BettingAPI.Models
-{
-    public class BetLine
-    {
-        public int Id { get; set; }
 
-        public int BetId { get; set; }
-        public Bet Bet { get; set; }
-
-        public int MatchId { get; set; }
-        public Match Match { get; set; }
-
-        public string SelectionType { get; set; }  // "Home", "Away", "Draw"
-
-        public decimal Odds { get; set; }
-    }
-}
