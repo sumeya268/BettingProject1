c2d0ca4e2bd2348302b59138d83be63c42b80dd0
aiahmed240 <ai677@live.mdx.ac.uk>
2026-04-15 15:50:32 +0100

    Update Sports.cs

--- Models/Sports.cs
+++ Models/Sports.cs
@@ -1,1 +1,12 @@
+namespace BettingAPI.Models
+{
+    public class Sport
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public List<Match> Matches { get; set; } = new();
+    }
+}
 
