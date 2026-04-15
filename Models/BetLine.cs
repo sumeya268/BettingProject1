a1111eb21e6a21b2aebfbc1c0a14de85ca2e9aef
mozillagit <mo1062@live.mdx.ac.uk>
2026-04-15 15:26:09 +0100

    Add BetLine model with properties for betting

--- Models/BetLine.cs
+++ Models/BetLine.cs
@@ -1,1 +1,17 @@
+namespace BettingAPI.Models
+{
+    public class BetLine
+    {
+        public int Id { get; set; }
 
+        public int BetId { get; set; }
+        public Bet Bet { get; set; }
+
+        public int MatchId { get; set; }
+        public Match Match { get; set; }
+
+        public string SelectionType { get; set; }  // "Home", "Away", "Draw"
+
+        public decimal Odds { get; set; }
+    }
+}
