6ec81b453dc3a21ace598c2e010aab4b805eebd1
Melvin170904 <melvinjenis@hotmail.com>
2026-04-15 15:18:52 +0100

    Update Bet.cs

--- Models/Bet.cs
+++ Models/Bet.cs
@@ -1,1 +1,17 @@
+namespace BettingAPI.Models
+{
+    public class Bet
+    {
+        public int Id { get; set; }
 
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public decimal Stake { get; set; }
+        public decimal PotentialReturn { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        public List<BetLine> BetLines { get; set; } = new();
+    }
+}
