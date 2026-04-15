68eec7266e4bcb6ba386c6eb81057b4383ce2c83
sumeya268 <SM3949@live.mdx.ac.uk>
2026-04-15 15:05:10 +0100

    editing

--- Models/Sports.cs
+++ Models/Sports.cs
@@ -1,11 +1,1 @@
-﻿namespace BettingAPI.Models
-{
-    public class Sport
-    {
-        public int Id { get; set; }
 
-        public string Name { get; set; } = null!;
-
-        public List<Match> Matches { get; set; } = new();
-    }
-}
