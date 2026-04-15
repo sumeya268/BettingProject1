e0c9d7482e0d3e24eb9f1aa7b119e7d087ea618f
sumeya268 <SM3949@live.mdx.ac.uk>
2026-04-15 15:05:31 +0100

    editing

--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,1 @@
-﻿namespace BettingAPI.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
 
-        public string Username { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string PasswordHash { get; set; } = null!;
-
-        public decimal Balance { get; set; }
-
-        public List<Bet> Bets { get; set; } = new();
-    }
-}
